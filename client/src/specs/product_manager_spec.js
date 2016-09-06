8b469aaa4786f63ef781371c45d31b4278dc906b
Joseph.Maher <joseph-maher@hotmail.co.uk>
2016-09-06 18:07:27 +0100

    Tests setup

---
+++ client/src/specs/product_manager_spec.js
@@ -0,0 +1,17 @@
+var FlightQuote = require('../models/product_manager.js')
+var assert = require('chai').assert
+
+
+describe("Product Manager Test", function(){
+
+  beforeEach(function(){
+    //code goes here
+  })
+
+  it('Test', function(){
+
+    assert.equal(true, true)
+  })
+
+
+})
